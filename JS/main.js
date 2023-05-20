fd8a143e65657bf84318572f1be08a94e8f47013
AzizbekQuvatov2009 <quvatovazizbek22@gmail.com>
2023-05-20 11:05:57 +0500

    JS_3-dars kodlari
    JS_3-dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+/* If,else;if,else if, else shart formulalar */
+
+/*let a = 10;
+let b =46;
+let c = 49;
+let d = 55;*/
+/*if (a == 10 & b == 46 & c == 49 & d == 5){
+    console.log("Shartlarning hammasi teng!")
+}else{
+    console.log("Shartlarning kamida bittasi teng emas!")
+}*/
+/*if ( a == 10 || b == 46 || c == 49 || d == 56){
+    console.log("Shartlarning bajarildai!")
+}else {
+    console.log("Hamma shartlar bajarilmadi!")
+}*/
+
+/* Data type: Number, Boolean, Undefined */
+/*let matn = 0;
+document.write(typeof matn);*/
+/*let rost = true;*/ /* boolean: true,false*/
+/*let rost = 45 == 45 ? true:false;
+document.write( rost);*/
+
+/*JS Object*/
+/*let arrayObject =[22,78,'matn',true,false,'js','son',99];
+console.log( arrayObject[3], arrayObject[4]);*/
+
+/*let oddiyObject = {ism: "Azizbek",yoshi:13, talabami:"yoq"};
+/!*oddiyObject: {key-value}*!/
+
+let talabami = "Yo'q , talaba emas"
+if (oddiyObject.yoshi > 17){
+    talabami = "Ha, talaba"
+}
+console.log( talabami);*/
+
+let oddiyObject = {qayer: "Uchqun",uy:150, tamirlanganmi:"yoq"};
+/*oddiyObject: {key-value}*/
+
+let tamirlanganmi = "Yo'q , tamirlanmagan"
+if (oddiyObject.uy < 150){
+    tamirlanganmi = "Ha, tamirlangan"
+}
+console.log( tamirlanganmi);
+
+
